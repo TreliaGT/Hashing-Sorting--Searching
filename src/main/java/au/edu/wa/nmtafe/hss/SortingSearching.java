08055d0623573a42a323b5d0b591aede4ed4befe
TreliaGT <v244682@tafe.wa.edu.au>
2019-09-13 15:28:49 +0800

    bubble sort

--- src/main/java/au/edu/wa/nmtafe/hss/SortingSearching.java
+++ src/main/java/au/edu/wa/nmtafe/hss/SortingSearching.java
@@ -13,14 +13,21 @@ import java.lang.reflect.Array;
  */
 public class SortingSearching {
 
+    /**
+     * Bubble sort the array
+     *
+     * @param array
+     * @return
+     */
     public int[] SortingArray(int[] array) {
+        int n = array.length;
         int temp;
-        for (int i = 1; i < array.length; i++) {
-            for (int j = i; j > 0; j--) {
-                if (array[j] < array[j - 1]) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
                     temp = array[j];
-                    array[j] = array[j - 1];
-                    array[j - 1] = temp;
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
                 }
             }
         }
